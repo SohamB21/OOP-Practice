b90ff030caf850521f6711a7de90ce40769dbf96
Soham Banik <soham0110banik@gmail.com>
2023-08-25 21:36:31 +0530

    Create GradesImplementation.java

---
+++ Grades/GradesImplementation.java
@@ -0,0 +1,12 @@
+package MyPackage;
+/* Q.6) Student Grades: The total grades of all 12th students regardless of their chosen pathway must be calculated. 
+ * The subjects vary depending on the choice of their pathway. For eg,  a student who has opted for the medical 
+ * pathway will get grades for PCMB. For non medical stream students will get grades for PCM, a business student will
+ * get grades for business studies, finance & accounting.*/
+
+public class GradesImplementation
+{
+    public static void main(String args[]){
+        System.out.println("Grades");
+    }
+}
